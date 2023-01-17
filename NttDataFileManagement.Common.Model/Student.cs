171725ceee0a8a35978b82770789fff2fe2fac01
USERSAD\abravogu <abravogu@emeal.nttdata.com>
2023-01-17 13:21:58 +0100

    Added student scaffolding implementation , with students classes in all layers closes #3

---
+++ NttDataFileManagement.Common.Model/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NttDataFileManagement.Common.Model
+{
+    public class Student
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Surname { get; set; } 
+        public DateTime Birthday { get; set; }
+        public int Age { get; set; }    
+
+
+    }
+}
